9f86d5382d406625aa70ff5b6e8da75ca3789e02
maishengli <shengli.mai@hotmail.com>
2026-01-15 16:23:57 +0800

    polish

--- code/train_rl.py
+++ code/train_rl.py
@@ -188,7 +188,7 @@ train_mode = True
 if train_mode:
     agent = DRLAgent(env = env_train_vm)
     # 检查是否存在已训练的模型，如果存在则加载继续训练
-    final_model_path = os.path.join('trained_models/', version+model_name, 'model2000.zip')
+    final_model_path = os.path.join('trained_models/', version+model_name, 'best_train_model.zip')
     if os.path.exists(final_model_path):
         print(f"load: {final_model_path}...")
         model_sac = SAC_MAE.load(final_model_path, env=env_train_vm, tensorboard_log=tensorboard_log_dir)
@@ -211,7 +211,7 @@ if train_mode:
     print("Training time: %.3f"%(end-start))
 
 
-model_path = os.path.join('trained_models/', version, model_name, 'best_model.zip')
+model_path = os.path.join('trained_models/', version, model_name, 'best_train_model.zip')
 start = time.time()
 results = DRLAgent.DRL_prediction_load_from_file(model_name='maesac',environment=test_trade_gym, cwd=model_path)
 end = time.time()
