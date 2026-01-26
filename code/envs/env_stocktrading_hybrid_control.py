d7ad51454e4ddd88b3cad252ff1328baba2bd385
SL Mai <shengli.mai@hotmail.com>
2026-01-26 12:46:43 +0800

    add warmup_steps

--- code/envs/env_stocktrading_hybrid_control.py
+++ code/envs/env_stocktrading_hybrid_control.py
@@ -105,6 +105,8 @@ class StockTradingEnv(gym.Env):
         self.short_prediction_model.eval()
         self.long_prediction_model.eval()
 
+        self.warmup_steps = 15 # 新增：预热步数
+
         # additional list
         self.print_additional_flag = print_additional_flag
         self.short_hidden_feature = []
@@ -389,12 +391,29 @@ class StockTradingEnv(gym.Env):
     def reset(self):
 
         if self.mode == 'train':            
-            self.time_windows_point += 1#remove for test
+            self.time_windows_point += 3#remove for test
+            if self.time_windows_point >= len(self.time_window_start):
+                self.time_windows_point = 0
             self.start_day = self.time_window_start[self.time_windows_point]
         else:
             self.start_day = self.time_window_start[0]
 
         self.day = self.start_day
+
+        # --- 方案2：状态预热实现 ---
+        self.short_hidden_feature = []
+        self.long_hidden_feature = []
+        
+        # 在正式开始 Episode 前，模拟运行若干步以预热 Transformer 特征
+        # 注意：预热期间不进行交易，不计入奖励
+        for _ in range(self.warmup_steps):
+            if self.day < self.df.index.unique().max() - 1:
+                self.data = self.df.loc[self.day, :]
+                self.info = self._initiate_info()
+                self.state = self._initial_state()
+                self.day += 1
+        # ------------------------
+
         self.data = self.df.loc[self.day, :]
         # self.covs = self.data['cov_list'].values[0]
 
