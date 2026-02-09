cbec66a938d2b6eea4232fd25009a9a2c8e8319f
SL Mai <shengli.mai@hotmail.com>
2026-02-09 10:21:30 +0800

    test-101449

--- code/train_rl.py
+++ code/train_rl.py
@@ -120,8 +120,8 @@ if __name__ == '__main__':
 
         # 训练强化学习代理,加载模型
         agent = DRLAgent(env = env_train_vm)
-        policy_kwargs = {"optimizer_kwargs": {"weight_decay": 1e-4},
-                         #"net_arch": [128, 128], # 与 d_model 保持一致，默认[256,256]
+        policy_kwargs = {#"optimizer_kwargs": {"weight_decay": 1e-4},
+                         "net_arch": [128, 128], # 与 d_model 保持一致，默认[256,256]
                          "use_sde": False
                         }#for MlpPolicy
         if load_pretrain:
