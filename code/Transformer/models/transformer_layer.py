c5cf1c49f156118b1853cbb413abf71536ab41ee
SL Mai <shengli.mai@hotmail.com>
2026-02-19 08:47:39 +0800

    Revert "remove 'normal' in transformer layer"
    This reverts commit 17a164ba08010aebafbff506f39af4c1465738a7.

--- code/Transformer/models/transformer_layer.py
+++ code/Transformer/models/transformer_layer.py
@@ -43,8 +43,8 @@ class EncoderLayer(nn.Module):
         d_ff = d_ff or 4 * d_model
         self.attention = attention
         self.mhfw = MultiheadFeedForward(d_model=d_model, n_heads=n_heads, ff_dim=d_ff, dropout=dropout, activation=activation)
-        # self.norm1 = nn.LayerNorm(d_model)
-        # self.norm2 = nn.LayerNorm(d_model)
+        self.norm1 = nn.LayerNorm(d_model)
+        self.norm2 = nn.LayerNorm(d_model)
         self.dropout = nn.Dropout(dropout)
 
     def forward(self, x, attn_mask=None):
@@ -54,12 +54,10 @@ class EncoderLayer(nn.Module):
         )
         x = x + self.dropout(new_x)
 
-        # y = x = self.norm1(x)
-        y = x
+        y = x = self.norm1(x)
         y = self.mhfw(y)
 
-        # return self.norm2(x + y), attn
-        return x + y, attn
+        return self.norm2(x + y), attn
 
 
 class Encoder(nn.Module):
@@ -67,7 +65,7 @@ class Encoder(nn.Module):
         super(Encoder, self).__init__()
         self.attn_layers = nn.ModuleList(attn_layers)
         self.conv_layers = nn.ModuleList(conv_layers) if conv_layers is not None else None
-        # self.norm = norm_layer
+        self.norm = norm_layer
 
     def forward(self, x, attn_mask=None):
         # x [B, L, D]
@@ -84,8 +82,8 @@ class Encoder(nn.Module):
                 x, attn = attn_layer(x, attn_mask=attn_mask)
                 attns.append(attn)
 
-        # if self.norm is not None:
-        #     x = self.norm(x)
+        if self.norm is not None:
+            x = self.norm(x)
 
         return x, attns
 
@@ -98,9 +96,9 @@ class DecoderLayer(nn.Module):
         self.self_attention = self_attention
         self.cross_attention = cross_attention
         self.mhfw = MultiheadFeedForward(d_model=d_model, n_heads=n_heads, ff_dim=d_ff, dropout=dropout, activation=activation)
-        # self.norm1 = nn.LayerNorm(d_model)
-        # self.norm2 = nn.LayerNorm(d_model)
-        # self.norm3 = nn.LayerNorm(d_model)
+        self.norm1 = nn.LayerNorm(d_model)
+        self.norm2 = nn.LayerNorm(d_model)
+        self.norm3 = nn.LayerNorm(d_model)
         self.dropout = nn.Dropout(dropout)
 
     def forward(self, x, cross, x_mask=None, cross_mask=None):
@@ -108,34 +106,32 @@ class DecoderLayer(nn.Module):
             x, x, x,
             attn_mask=x_mask
         )[0])
-        # x = self.norm1(x)
+        x = self.norm1(x)
 
         x = x + self.dropout(self.cross_attention(
             x, cross, cross,
             attn_mask=cross_mask
         )[0])
 
-        # y = x = self.norm2(x)
-        y = x
+        y = x = self.norm2(x)
         y = self.mhfw(y)
 
-        # return self.norm3(x + y)
-        return x + y
+        return self.norm3(x + y)
 
 
 class Decoder(nn.Module):
     def __init__(self, layers, norm_layer=None, projection=None):
         super(Decoder, self).__init__()
         self.layers = nn.ModuleList(layers)
-        # self.norm = norm_layer
+        self.norm = norm_layer
         self.projection = projection
 
     def forward(self, x, cross, x_mask=None, cross_mask=None):
         for layer in self.layers:
             x = layer(x, cross, x_mask=x_mask, cross_mask=cross_mask)
 
-        # if self.norm is not None:
-        #     x = self.norm(x)
+        if self.norm is not None:
+            x = self.norm(x)
 
         if self.projection is not None:
             x = self.projection(x)
