5f7317f4aa343f85a0cbaa8f5a2599c77c7d3da4
SL Mai <shengli.mai@hotmail.com>
2026-02-23 09:13:45 +0800

    test-0757

--- code/MySAC/SAC/MAE_SAC.py
+++ code/MySAC/SAC/MAE_SAC.py
@@ -203,9 +203,10 @@ class SAC(SAC_SB3):
             print("Successfully load pretrained MAE model...", transformer_path)
         else:
             print("Successfully initialize MAE model...")
-
+        
+        #for MAE
         self.transformer_device = transformer_device
-        self.transformer_optim = th.optim.Adam(self.state_transformer.parameters(), lr=1e-5, weight_decay=1e-4)
+        self.transformer_optim = th.optim.AdamW(self.state_transformer.parameters(), lr=1e-5, weight_decay=1e-4)
         self.transformer_criteria = th.nn.MSELoss()
 
         self.critic_alpha = critic_alpha
@@ -299,7 +300,7 @@ class SAC(SAC_SB3):
             # Note: we optimize the log of the entropy coeff which is slightly different from the paper
             # as discussed in https://github.com/rail-berkeley/softlearning/issues/37
             self.log_ent_coef = th.log(th.ones(1, device=self.device) * init_value).requires_grad_(True)
-            self.ent_coef_optimizer = th.optim.Adam([self.log_ent_coef], lr=self.lr_schedule(1), weight_decay=1e-4)
+            self.ent_coef_optimizer = th.optim.Adam([self.log_ent_coef], lr=self.lr_schedule(1))
         else:
             # Force conversion to float
             # this will throw an error if a malformed string (different from 'auto')
