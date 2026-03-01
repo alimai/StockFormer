0feca360763bf93db0068c6a15b52f822c6604dc
SL Mai <shengli.mai@hotmail.com>
2026-03-01 19:44:29 +0800

    polish --2

--- code/utils/config.py
+++ code/utils/config.py
@@ -97,7 +97,7 @@ MAESAC_PARAMS = {
     "buffer_size": 80000,#用于存储环境的"经验"(Obs, Action, Reward, Next_Obs, Done)
     "buffer_max_load": 50000,# 【新增】指定从旧 Buffer 加载的数据条数，None 为全部加载
     "learning_starts": 1000,
-    "learning_rate": 1e-4,#所有模块初始学习率,会被 _update_learning_rate() 动态调整
+    "learning_rate": 1e-4,#所有模块初始学习率
     "ent_coef": "auto_0.001",#0.001#key
     
     # MAE Transformer 架构参数 - 保持不变（与预训练模型兼容）
