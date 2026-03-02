ea2e75ad1a9e91b94e2c8e5116aec06e1ddd676d
SL Mai <shengli.mai@hotmail.com>
2026-03-02 22:40:04 +0800

    test-2200

--- code/envs/env_stocktrading_hybrid_control.py
+++ code/envs/env_stocktrading_hybrid_control.py
@@ -333,7 +333,7 @@ class StockTradingEnv(gym.Env):
             market_value_growth_ratio = np.sum(avg_prices) / np.sum(zero_day_prices) - 1.0
             self.reward = asset_for_reward_new / begin_total_asset - 1.0
             self.reward = self.reward + (self.reward - market_value_growth_ratio) * 1.5
-            self.reward = np.clip(self.reward, -0.03, 0.03)
+            self.reward = np.clip(self.reward, -0.05, 0.05)
             self.reward *= self.reward_scaling
             # if self.rewards_memory:
             #     self.reward -= np.average(self.rewards_memory[-5:]) * 0.1          
