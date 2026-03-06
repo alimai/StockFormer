25f465a591a6e52e2454b3d9f77a1e43193bfd7d
SL Mai <shengli.mai@hotmail.com>
2026-03-07 06:42:47 +0800

    test 0254/0422

--- code/train_rl.py
+++ code/train_rl.py
@@ -185,7 +185,7 @@ if __name__ == '__main__':
                                     eval_log_dir=log_path_eval,#callback 路径
                                     model_dir=model_path,
                                     eval_env=env_eval_vm,
-                                    total_timesteps=69000)
+                                    total_timesteps=99000)
         end = time.time()
         print("Training time: %.3f"%(end-start))
 
