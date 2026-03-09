2e19f3a191d978a7d5b2f2c5466e910e7eaa7ac7
SL Mai <shengli.mai@hotmail.com>
2026-03-09 09:46:18 +0800

    update log reward

--- code/envs/env_stocktrading_hybrid_control.py
+++ code/envs/env_stocktrading_hybrid_control.py
@@ -321,22 +321,16 @@ class StockTradingEnv(gym.Env):
 
             avg_prices = first_day_prices * 0.3 + fifth_day_prices * 0.7
             asset_for_reward_new = self.env_info[0] + np.sum(avg_prices * shares)
-
-            market_value_growth_ratio = np.sum(avg_prices) / np.sum(zero_day_prices) - 1.0
             reward_absolut = asset_for_reward_new / begin_total_asset - 1.0
+            market_value_growth_ratio = np.sum(avg_prices) / np.sum(zero_day_prices) - 1.0
             reward_relative = (reward_absolut - market_value_growth_ratio) * 1.5
+
             reward_absolut *= self.reward_scaling
             reward_relative *= self.reward_scaling
-            # if config.struct_base_flag:
-            # 使用对数形式：保留符号，使用 log1p 计算对数
-            if reward_absolut > -1.0:#对应reward_scaling缩放前-0.01
-                reward_absolut = np.sign(reward_absolut) * np.log1p(np.abs(reward_absolut))
-            if reward_relative > -1.0:#对应reward_scaling缩放前-0.01
-                reward_relative = np.sign(reward_relative) * np.log1p(np.abs(reward_relative))
-            # else:
-            #     reward_absolut = np.sign(reward_absolut) * np.log1p(np.abs(reward_absolut))
-            #     reward_relative = np.sign(reward_relative) * np.log1p(np.abs(reward_relative))
             self.reward = reward_absolut + reward_relative
+            # 使用对数形式：保留符号，使用 log1p 计算对数
+            if self.reward > -1.0:#对应reward_scaling缩放前-0.01
+                self.reward = np.sign(self.reward) * np.log1p(np.abs(self.reward))
 
 
             self.actions_memory.append(actions)
