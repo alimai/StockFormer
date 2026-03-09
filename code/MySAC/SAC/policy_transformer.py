395c0220a55bc9b7f4e5f3c98cc6d10b1e266cba
SL Mai <shengli.mai@hotmail.com>
2026-03-09 16:37:25 +0800

    test1540

--- code/MySAC/SAC/policy_transformer.py
+++ code/MySAC/SAC/policy_transformer.py
@@ -100,14 +100,14 @@ class policy_transformer_stock_atten2(nn.Module): # attention(long, short), atte
         # 2) Output Processing
         #fused_output = torch.cat((relational_hybrid_feature, temporal_hybrid_feature), dim=-1)
         #fused_output = torch.cat((relational_feature, temporal_feature_long, temporal_feature_short, additional_feature), dim=-1)
-        fused_output_adapted = self.projection_output(hybrid_feature) # [B, N, 128]
+        #fused_output_adapted = self.projection_output(hybrid_feature) # [B, N, 128]
         
         # Late Fusion (Skip Connection with Clean Context)
         # 再次拼接: Processed Context (128)与附加上下文（Tech + Date）
         if update_type==2:
-            combined_feature = self.dropout(torch.cat((fused_output_adapted, additional_feature), dim=-1))  # [B, N, 128+additional_dim]
+            combined_feature = self.dropout(torch.cat((hybrid_feature, additional_feature), dim=-1))  # [B, N, 128+additional_dim]
         else:
-            combined_feature = torch.cat((fused_output_adapted, self.dropout(additional_feature)), dim=-1)  # [B, N, 128+additional_dim]
+            combined_feature = torch.cat((hybrid_feature, self.dropout(additional_feature)), dim=-1)  # [B, N, 128+additional_dim]
         return combined_feature
 
     def forward_front(self, relational_feature, temporal_feature_short, temporal_feature_long, additional_feature, mask=None):
