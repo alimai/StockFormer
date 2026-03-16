eff9c9079f4fc83d58cba4409a86a32092f19176
SL Mai <shengli.mai@hotmail.com>
2026-03-16 18:11:47 +0800

    test 1754

--- code/MySAC/SAC/policy_transformer.py
+++ code/MySAC/SAC/policy_transformer.py
@@ -35,11 +35,11 @@ class policy_transformer_stock_atten2(nn.Module): # attention(long, short), atte
         self.dropout = nn.Dropout(dropout)
 
         if config.fix_seed % 3 == 0:
-            self.dropout1 = nn.Dropout(dropout*1.5)
+            self.dropout1 = nn.Dropout(dropout*1.3)
             self.dropout2 = nn.Dropout(dropout*0.1)
         elif config.fix_seed % 3 == 1:
             self.dropout1 = nn.Dropout(dropout*0.1)
-            self.dropout2 = nn.Dropout(dropout*1.5)
+            self.dropout2 = nn.Dropout(dropout*1.3)
         else:
             self.dropout1 = nn.Dropout(dropout)
             self.dropout2 = nn.Dropout(dropout)
