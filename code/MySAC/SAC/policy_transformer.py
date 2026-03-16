b9ef71d2dbe5a020891b5491740d67a1f4cd458e
SL Mai <shengli.mai@hotmail.com>
2026-03-16 13:25:08 +0800

    add holding; test1322

--- code/MySAC/SAC/policy_transformer.py
+++ code/MySAC/SAC/policy_transformer.py
@@ -23,7 +23,7 @@ class policy_transformer_stock_atten2(nn.Module): # attention(long, short), atte
         self.device = device
         self.hidden_out = hidden_out
         self.atten_dim = hidden_out // config.scale_ratio # attention内部维度，通常设置为hidden_out的整数倍
-        self.add_dim = additional_dim-1 #去掉holding部分
+        self.add_dim = additional_dim#-1 #去掉holding部分
         
         self.attention1 = AttentionLayer(FullAttention(False, attention_dropout=dropout,
                                       output_attention=output_attention), self.atten_dim, n_heads)
@@ -34,15 +34,15 @@ class policy_transformer_stock_atten2(nn.Module): # attention(long, short), atte
         self.norm2 = nn.LayerNorm(self.atten_dim)
         self.dropout = nn.Dropout(dropout)
 
-        # if config.fix_seed % 2 == 0:
-        #     self.dropout1 = nn.Dropout(dropout*1.5)
-        #     self.dropout2 = nn.Dropout(dropout*0.1)
-        # elif config.fix_seed % 2 == 1:
-        #     self.dropout1 = nn.Dropout(dropout*0.1)
-        #     self.dropout2 = nn.Dropout(dropout*1.5)
-        # else:
-        self.dropout1 = nn.Dropout(dropout)
-        self.dropout2 = nn.Dropout(dropout)
+        if config.fix_seed % 2 == 0:
+            self.dropout1 = nn.Dropout(dropout*1.5)
+            self.dropout2 = nn.Dropout(dropout*0.1)
+        elif config.fix_seed % 2 == 1:
+            self.dropout1 = nn.Dropout(dropout*0.1)
+            self.dropout2 = nn.Dropout(dropout*1.5)
+        else:
+            self.dropout1 = nn.Dropout(dropout)
+            self.dropout2 = nn.Dropout(dropout)
 
         # 特征融合后投影回 hidden_out
         self.projection_input = nn.Sequential(
@@ -68,7 +68,7 @@ class policy_transformer_stock_atten2(nn.Module): # attention(long, short), atte
         # additional_feature: [B, N, add_dim] (Tech + Date)
               
         # 处理输入特征 (Refinement)
-        add_feature = additional_feature[:, :, :-1] #去掉holding部分
+        add_feature = additional_feature#[:, :, :-1] #去掉holding部分
         if config.struct_type % 2 == 1:
             base_feature = temporal_feature_long
             minor_feature = relational_feature
